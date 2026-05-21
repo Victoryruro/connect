df2efc2f0e64efefdc02c7882a5ec19b8dfb5546
Victory Diemuodeke <Victorydiemuodeke@gmail.com>
2026-05-21 15:09:37 +0100

    fixing the route

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import React from 'react'
-import './App.css'
-import Header from './Components/header/Header'
-import Homepages from './Components/home/Homepage'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
-import Footer from './Components/footer/Footer'
-import TechPage from './Components/Techpage/TechPage'
-import Fashion from './Components/Fashion/Fashion'
+import React from 'react';
+import './App.css';
+import Header from './Components/header/Header';
+import Homepages from './Components/home/Homepage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './Components/footer/Footer';
+import TechPage from './Components/Techpage/TechPage';
+import Fashion from './Components/Fashion/Fashion';
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-      <Routes>
-
-        <Route path="/" Component={Homepages} />
-        <Route path="/TechPage" Component={TechPage} />
-        <Route path="/Fashion" Component={Fashion} />
-        {/* <Route path="/fashion" element={<Fashion />} />
-        <Route path="/tech" element={<Tech />} />
-        <Route path="/music" element={<Music />} />
-        <Route path="/review" element={<Review />} /> */}
-      </Routes>
-        
-      </BrowserRouter>
-        <Footer />
+    <Router basename="/ConnectHQ">   {/* ← This is the most important fix */}
+      <Header />
       
+      <Routes>
+        <Route path="/" element={<Homepages />} />
+        <Route path="/TechPage" element={<TechPage />} />
+        <Route path="/Fashion" element={<Fashion />} />
+      </Routes>
 
+      <Footer />
+    </Router>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
